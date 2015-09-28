9af20e1fdf81baf2a579e2bfaafb1cce4108cfca
vmartinezf <vanessa_1895@msn.com>
2015-09-28 14:37:55 +0200

    nuevo

--- calcoo.py
+++ calcoo.py
@@ -5,19 +5,22 @@
 
 import sys
 
+
 class Calculadora():
 
-    def suma (self,num1, num2):
+    def suma(self, num1, num2):
         return num1 + num2
 
-    def resta (self,num1, num2):
-    	return num1 - num2
+    def resta(self, num1, num2):
+        return num1 - num2
+
 
 def main():
     pass
 
+
 #convierto el operando sea int o float a float
-def numerical_float (operando):
+def numerical_float(operando):
     return float(operando)
 
 if __name__ == "__main__":
@@ -27,8 +30,8 @@ if __name__ == "__main__":
     numero2 = sys.argv[3]
 
     try:
-        num1 = numerical_float (numero1)
-        num2 = numerical_float (numero2)
+        num1 = numerical_float(numero1)
+        num2 = numerical_float(numero2)
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
@@ -37,4 +40,3 @@ if __name__ == "__main__":
         print (result.suma(num1, num2))
     elif (operacion == "resta"):
         print(result.resta(num1, num2))
-        
