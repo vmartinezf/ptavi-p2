c7c292590e0f061b17cf3608b5ad59c31c741c74
vmartinezf <vanessa_1895@msn.com>
2015-09-28 16:47:46 +0200

    inicio a ficheros

---
+++ calcplus.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import sys
+from calcoohija import CalculadoraHija
+from calcoohija import numerical_float
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    fichero = sys.argv[1]
+    fich = open(fichero, 'r')
+    lines = fich.readlines()
+    for line in lines:
+        lineas = line.split()
+        for operandos in lineas:
+            operando = operandos.split(',')
+            operacion = operando[0]
+            numero = numerical_float(operando[1])
+            result = CalculadoraHija()
+            for i in operando[2:]:
+                number = int(i)
+                num2 = numerical_float(number)        
+                if (operacion == "suma"):   
+                    numero = result.suma(numero, num2)
+                elif (operacion == "resta"):
+                    numero = result.resta(numero, num2)
+                elif (operacion == "multiplica"):
+                    numero = result.multiplicacion(numero, num2)
+                elif (operacion == "divide"):
+                    numero = result.division(numero, num2)
+            print (numero)      
