0cd2685a0698b5531b9b39a1e08ec30cf65af182
vmartinezf <vanessa_1895@msn.com>
2015-10-02 23:37:51 +0200

    calcoo mejorado

--- calcoo.py
+++ calcoo.py
@@ -23,20 +23,29 @@ def main():
 def numerical_float(operando):
     return float(operando)
 
+
+def resultado(operacion, num1, num2):
+    result = Calculadora()
+    if (operacion == "suma"):    
+        total=result.suma(num1, num2)
+    elif (operacion == "resta"):
+        total=result.resta(num1, num2)
+    else:
+        sys.exit ("Error: the operation is incorrect")
+    return total
+
+
+
 if __name__ == "__main__":
-
-    numero1 = sys.argv[1]
-    operacion = sys.argv[2]
-    numero2 = sys.argv[3]
-
+    try:
+        numero1 = sys.argv[1]
+        operacion = sys.argv[2]
+        numero2 = sys.argv[3]
+    except IndexError:
+        sys.exit("Error: The number of parameters is incorrect")
     try:
         num1 = numerical_float(numero1)
         num2 = numerical_float(numero2)
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-
-    result = Calculadora()
-    if (operacion == "suma"):    
-        print (result.suma(num1, num2))
-    elif (operacion == "resta"):
-        print(result.resta(num1, num2))
+    print (resultado(operacion, num1, num2))
