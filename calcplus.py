9ec25f2d412c2170c36873ee795959e05602a763
vmartinezf <vanessa_1895@msn.com>
2015-10-04 10:33:54 +0200

    calcplus pep8

--- calcplus.py
+++ calcplus.py
@@ -5,33 +5,40 @@
 import sys
 from calcoohija import CalculadoraHija
 from calcoohija import numerical_float
+from calcoohija import resultado
 
 
 def main():
     pass
 
 
-if __name__ == "__main__":
-    fichero = sys.argv[1]
-    fich = open(fichero, 'r')
-    lines = fich.readlines()
-    for line in lines:
-        lineas = line.split()
-        for operandos in lineas:
+def resultados_operaciones(lineas):
+    for operandos in lineas:
             operando = operandos.split(',')
             operacion = operando[0]
             numero = numerical_float(operando[1])
-            result = CalculadoraHija()
             for i in operando[2:]:
                 number = int(i)
-                num2 = numerical_float(number)        
-                if (operacion == "suma"):   
-                    numero = result.suma(numero, num2)
-                elif (operacion == "resta"):
-                    numero = result.resta(numero, num2)
-                elif (operacion == "multiplica"):
-                    numero = result.multiplicacion(numero, num2)
-                elif (operacion == "divide"):
-                    numero = result.division(numero, num2)
-            print (numero)      
+                num2 = numerical_float(number)
+                total = resultado(operacion, numero, num2)
+                numero = numerical_float(total)
+    return total
+
+
+def lineas_fichero(lines):
+    for line in lines:
+        lineas = line.split()
+        total = resultados_operaciones(lineas)
+        print (total)
+
+
+def resultados_fichero(fich):
+    lines = fich.readlines()
+    lineas_fichero(lines)
+
+
+if __name__ == "__main__":
+    fichero = sys.argv[1]
+    fich = open(fichero, 'r')
+    resultados_fichero(fich)
     fich.close()
