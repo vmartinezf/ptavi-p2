d01d0174f927e26df0e53b37c1d88058f9d0deba
vmartinezf <vanessa_1895@msn.com>
2015-10-05 10:20:20 +0200

    calcplus pep8 otro

--- calcplus.py
+++ calcplus.py
@@ -10,14 +10,14 @@ from calcoohija import resultado
 
 def resultados_operaciones(lineas):
     for operandos in lineas:
-            operando = operandos.split(',')
-            operacion = operando[0]
-            numero = numerical_float(operando[1])
-            for i in operando[2:]:
-                number = int(i)
-                num2 = numerical_float(number)
-                total = resultado(operacion, numero, num2)
-                numero = numerical_float(total)
+        operando = operandos.split(',')
+        operacion = operando[0]
+        numero = numerical_float(operando[1])
+        for i in operando[2:]:
+            number = int(i)
+            num2 = numerical_float(number)
+            total = resultado(operacion, numero, num2)
+            numero = numerical_float(total)
     return total
 
 
